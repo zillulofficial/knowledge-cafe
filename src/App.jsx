6ecb1dea39de3df2d3b9fcdfa5ce72abea2d54ac
zillulofficial <zillul.hakim100@gmail.com>
2024-03-16 11:55:41 +0600

    fake data created and fetched

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Blogs from './Components/Blogs/Blogs'
+import Header from './Components/Header/Header'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-3xl'>Knowledge Cafe</h1>
-      
+      <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
